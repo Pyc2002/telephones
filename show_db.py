f69833c328c0b6b7108d8af2b89317704aaa1e5c
Andrey Sokolov <sokolqe@gmail.com>
2022-12-15 21:59:24 +0300

    search and show_db

--- show_db.py
+++ show_db.py
@@ -2,12 +2,8 @@ from constants import DATA_BASE
 import csv
 
 
-def search_by(atr: str, search_data: str):
+def show():
     with open(DATA_BASE, "r", newline='', encoding='windows-1251') as csvfile:
         reader = csv.DictReader(csvfile, delimiter="|")
-        data = []
         for row in reader:
-            if search_data in row[atr]:
-                data.append(row)
-        for i in data:
-            print(*i.values())
+            print(row)
